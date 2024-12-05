4b9c7bada850aed3be785b3441bfcf9263082d9b
nazeranassser <nazera.nasser@icloud.com>
2024-12-05 15:38:04 +0300

    update

---
+++ Backend_Blogs/app/Http/Controllers/CommentController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Comment;
+
+use Illuminate\Http\Request;
+
+class CommentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $comments = Comment::all(); // جلب جميع التعليقات
+        return response()->json($comments);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request) {
+        $comment = Comment::create($request->all());
+        return response()->json($comment, 201);
+    }
+    
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
